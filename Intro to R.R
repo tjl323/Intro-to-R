a5069860a5a3343203d1bf2754b242da081f631c
Tommy Lewis <tjl323@lehigh.edu>
2019-09-18 09:33:58 -0400

    edited header

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,10 @@
-# R script to accopany Intro to R
-# Copy for RStudent 2019
+# R script to accompany Intro to R
+#
+#Written by Tommy Lewis
+
+# Copy for Tommy Lewis
+
+#Customized by Tommy Lewis
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
@@ -7,3 +12,5 @@
 # 
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
+#Character
+name <-"Tommy"
