eb7ecdd41cb05f9c5ba3487301ace4ce906d3464
Tommy Lewis <tjl323@lehigh.edu>
2019-10-30 21:09:55 -0400

    Resume

---
+++ AboutMe.R
@@ -0,0 +1,58 @@
+# AboutMe
+
+EDUCATION  
+
+Bachelor of Arts, Finance Major | Anticipated 2023 LEHIGH UNIVERSITY,Bethlehem, PA
+
+THE HOTCHKISS SCHOOL, Lakeville, CT ** |** May 201
+
+**EXPERIENCE**
+  
+  Lifeguard | Palisades Country Day Camp
+
+**CLOSTER, NJ | Summers 2017 - 2019**
+  
+  - Monitored children ages 18 months to 8 years of age during free swim activities.
+- Conducted age appropriate swimming lessons with an emphasis on safety.
+- Communicated with parents and counselors to provide updates on each child&#39;s progress throughout the summer.
+  
+  **Dorm Proctor | The Hotchkiss School**
+  
+  **LAKEVILLE, CT**  **| September 2018 - May 2019**
+  
+  - Dorm leader for underclassman dorm.
+- Provided support, counsel and guidance to 30 students in the dormitory.
+- Created a welcoming, safe and friendly environment both in the dorm and in the school community.
+- Ensured proctees adhered to rules and regulations of both the school and the dormitory.
+
+
+**Teaching Assistant, Human Development | The Hotchkiss School**
+  
+  **LAKEVILLE**** , **** CT **** | September **** 2018 **** – May 2019**
+  
+  - Assisted faculty in leading section of 10th grade Human Development course.
+- Fostered peer development of critical-thinking and decision making skills essential for making reasonable and safe decisions.
+- Honed skills required to facilitate discussions about physical and mental health, sexuality, identity, drugs and alcohol, and relationships.
+- Mentored students on appropriate ways to navigate the pressures faced by high school students.
+
+
+**Tour Guide | The Hotchkiss School**
+  
+  **LAKEVILLE, CT**  **| September**  **2014**  **– May 2019**
+  
+  - Served as Senior Head Tour Guide.
+- Trained new tour guides on most effective ways to conduct tours.
+- Acted as an extension of the school's admissions office by providing school tours to prospective students and their families as well as special guests of the school.
+- Attended off-site admissions receptions to advocate for Hotchkiss and talk about my Hotchkiss experience.
+
+ 
+**LEADERSHIP AND VOLUNTEERING**
+
+**Lehigh University Varsity Football Team**
+
+**NCAA DIVISION IAA TEAM MEMBER | September 2019 - Present**
+
+Devoted 20+ hours per week to practice, training, competition and travel schedule.
+
+
+**The Hotchkiss School**
